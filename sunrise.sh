bb24c44d252cc91d16127eb14c60c33256079580
L3viathan <git@l3vi.de>
2014-05-14 18:31:18 +0200

    stuff

--- sunrise.sh
+++ sunrise.sh
@@ -9,3 +9,4 @@ say $(curl -L api.l3vi.de/motd.txt 2>/dev/null)
 curl -o heute.mov `curl -ss -L http://www.zdf.de/ZDFmediathek/100sec | grep mov | head -n 1|perl -pe 's/.*(http.*mov).*/$1/g'` >/dev/null 2>&1
 eval $player heute.mov >/dev/null 2>&1
 ./mensa.sh >/dev/null 2>&1
+say "Have a nice day."
